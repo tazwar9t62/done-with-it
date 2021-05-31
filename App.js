2ba19d79ff7ed9ae616a3a6cbcf0f3629d1405c7
Tazwar Islam <5572533-tazwar9t6@users.noreply.gitlab.com>
2021-06-01 00:14:15 +0600

    seepy

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View,  SafeAreaView,Image } from "react-native";
 
 export default function App() {
+  const handlePass = () => {
+    console.log("link clicked")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hey Tazwar you app working! yaay</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePass()}>Hey Tazwar you app working! yaay la la la la</Text>
+      {/* <Image source={require("./assets/favicon.png")} /> */}
+      <Image blurRadius={0.5} source={
+        {
+          width: 200,
+          height: 200,
+          uri:"https://picsum.photos/seed/picsum/200/300"
+
+        }
+      } />
+    
+
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
