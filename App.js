982466d8f86cc33649ad039e52ad38fa9b1d5546
Tazwar Islam <5572533-tazwar9t6@users.noreply.gitlab.com>
2021-06-01 23:44:27 +0600

    btn

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View,  SafeAreaView,Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
 export default function App() {
   const handlePass = () => {
-    console.log("link clicked")
-  }
+    console.log("link clicked");
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text onPress={handlePass()}>Hey Tazwar you app working! yaay la la la la</Text>
+      <Button
+        onPress={() => console.log("button clicked")}
+        title="Click me"
+        color="red"
+      ></Button>
+      <TouchableOpacity onPress={() => console.log("image clicked")}>
+        <Image
+          blurRadius={0.5}
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/seed/picsum/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <Text onPress={handlePass()}>
+        Hey Tazwar you app working! yaay la la la la
+      </Text>
       {/* <Image source={require("./assets/favicon.png")} /> */}
-      <Image blurRadius={0.5} source={
-        {
-          width: 200,
-          height: 200,
-          uri:"https://picsum.photos/seed/picsum/200/300"
-
-        }
-      } />
-    
-
-
     </SafeAreaView>
   );
 }
